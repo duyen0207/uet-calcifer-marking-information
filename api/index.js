98759d8d0ae8bb73759f7b022c3cc870caf7f26c
duyen0207 <duyeenlee2072001@gmail.com>
2023-02-27 14:34:11 +0700

    fix: success: promise error

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+// get the client
+const mysql = require("mysql2");
 const cron = require("node-cron");
 let shell = require("shelljs");
 
@@ -6,12 +8,22 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const multer = require("multer"); // v1.0.5
 const cors = require("cors");
-const mysql = require("mysql");
 
-const { notEmpty, resetSuccessCount, formatJSON } = require("./utils/utils");
 const { exportJSONFile } = require("./utils/handleFile");
-const { readReport } = require("./cypress/results/readReport");
-const { ScoringSubmissions } = require("./utils/scoring");
+const { readReport } = require("./utils/readReport");
+const { AnalysisReportAndUpdateScore, getNotScoredSubmissions } = require("./utils/scoring");
+
+// create the connection to database
+// connect to database
+const config = {
+  connectionLimit: 10,
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "uet_calcifer",
+};
+const database = mysql.createPool(config);
+const promiseDatabase = database.promise();
 
 const app = express();
 const port = 3001;
@@ -31,200 +43,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// connect to database
-const config = {
-  connectionLimit: 10,
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "uet_calcifer",
-};
-const database = mysql.createPool(config);
-const cronDatabase = mysql.createPool(config);
-
 // routes-------------------------------------------------------
 app.get("/", (req, res) => {
-  ejs.renderFile("views/ssr.ejs", {}, {}, function (err, template) {
-    if (err) {
-      console.log("data query error: ", err.code, err?.sqlMessage, err?.sqlState);
-    } else {
-      res.end(template);
-    }
-  });
+  res.send("<h3>Hello this is auto scoring system!</h3>");
 });
-
-// load data
-app.get("/data/filter", (req, res) => {
-  database.getConnection(function (err, tempConnection) {
-    if (err) {
-      res.send("Error occured.");
-    }
-
-    const isMarked = req.query?.isMarked;
-    const search = req.query?.search;
-
-    const sql = "CALL Proc_Submission_GetNotScoredAndSearch(?)";
-
-    database.query(sql, search, function (err, result, fields) {
-      if (err) {
-        console.log("Error is: ", err.code);
-        return;
-      }
-      const data = result;
-      const output = {
-        totalRecords: result.length,
-        data: data[0],
-      };
-
-      // console.log("data ", data[0]);
-      res.json(output);
-      tempConnection.release((error) => (error ? reject(error) : resolve()));
-    });
-  });
-});
-// load submission report
-app.get("/data/submission-report", (req, res) => {
-  database.getConnection(function (err, tempConnection) {
-    if (err) {
-      res.send("Error occured.");
-    }
-
-    const submissionId = req.query?.submissionId;
-
-    if (notEmpty(submissionId)) {
-      const dataQuery = "CALL Proc_SubmissionReport_GetOf(?)";
-
-      database.query(dataQuery, submissionId, function (err, result, fields) {
-        if (err) {
-          console.log("Error is: ", err.code);
-        }
-
-        res.json(result);
-        tempConnection.release((error) => (error ? reject(error) : resolve()));
-      });
-    } else res.json((message = "không tìm được"));
-  });
-});
-
-// update score
-var errors = [];
-var updateSuccessCount = 0;
-var insertSuccessCount = 0;
-
-function pushMarkingData(data) {
-  resetSuccessCount();
-  return new Promise((resolve) => {
-    for (let i = 0; i < data.length; i++) {
-      // console.log("đang chạy đến: ", i);
-      if (notEmpty(data[i].testcaseResult) && notEmpty(data[i].submissionId)) {
-        const updateQuery = "CALL Proc_Submission_UpdateScoreResult(?,?,?)";
-
-        database.query(
-          updateQuery,
-          data[i].testcaseResult,
-          data[i].score,
-          data[i].submissionId,
-          function (err, result, fields) {
-            if (err) {
-              console.log(err.code);
-              res.send("Error occured.", err.code);
-              errors.push({
-                submissionId: data[i].submissionId,
-                error: `Update lỗi: ${err.code}`,
-              });
-            } else {
-              updateSuccessCount += result?.affectedRows;
-              console.log("update: ", updateSuccessCount, result?.message);
-            }
-            // ket thuc promise
-            if (i == data.length - 1) {
-              console.log("update resolve.");
-              resolve(errors);
-            }
-          }
-        );
-      } else {
-        if (i == data.length - 1) {
-          console.log("update resolve.");
-          resolve(errors);
-        }
-      }
-    }
-  });
-}
-
-function pushSubmissionReportData(data) {
-  insertSuccessCount = 0;
-  return new Promise((resolve) => {
-    for (let i = 0; i < data.length; i++) {
-      const submissionReports = data[i]?.submissionReports;
-      // nếu ko có test sai
-      if (!submissionReports) {
-        console.log("không có test case nào sai.");
-        if (i == data.length - 1) resolve(errors);
-      }
-      // nếu có test sai
-      else {
-        for (let j = 0; j < submissionReports.length; j++) {
-          const insertReportQuery = `INSERT submission_report
-                (SubmissionId, ErrorTestcaseOrder, ErrorReport)
-                VALUES ('${data[i].submissionId}', ${
-            submissionReports[j].errorTestcaseOrder
-          }, '${formatJSON(submissionReports[j].errorReport)}');`;
-
-          database.query(insertReportQuery, function (err, result, fields) {
-            if (err) {
-              if (err.code == "ER_DUP_ENTRY") {
-                errors.push({
-                  submissionId: data[i].submissionId,
-                  testcaseOrder: submissionReports[j].errorTestcaseOrder,
-                  error:
-                    "Dữ liệu đã tồn tại, không thể insert submission report.",
-                });
-                console.log("lỗi sai: ", j);
-              }
-            } else {
-              insertSuccessCount += result?.affectedRows;
-            }
-            // kết thúc promise
-            if (i == data.length - 1 && j == submissionReports.length - 1) {
-              resolve(errors);
-            }
-          });
-        }
-      }
-    }
-  });
-}
-
-app.put("/data", (req, res) => {
-  console.log("hello, this is marking: ");
-
-  database.getConnection(function (err, tempConnection) {
-    const data = req.body;
-
-    if (err) {
-      res.send("Error occured.", err.code);
-    }
-
-    pushMarkingData(data).then((results) => {
-      pushSubmissionReportData(data).then((results) => {
-        console.log("aaaaaaa: ");
-        const output = {
-          message: `Update marking success: ${updateSuccessCount}/${data.length} records, Insert submission reports success: ${insertSuccessCount} records`,
-          errors: {
-            totals: results.length,
-            details: results,
-          },
-        };
-        res.json(output);
-        tempConnection.release((error) => (error ? reject(error) : resolve()));
-        return results;
-      });
-    });
-  });
-});
-
 // -------------------------------------------------------------------
 // handle 404 not found
 app.use((req, res, next) => {
@@ -235,55 +57,34 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-// getNotScoredSubmissions(database);
-
+// -----------------------------------------------------------------
 cron.schedule("*/5 * * * * *", function () {
   console.log("Cron job==============================================");
-  getNotScoredSubmissions(cronDatabase);
-  // if (shell.exec("node cronJob/cronTest.js").code !== 0) {
-  //   console.log("Some thing went wrong!");
-  // }
+  Scoring(promiseDatabase);
 });
 
 // get data function------------------------------------
-function getNotScoredSubmissions(database) {
-  database.getConnection(function (err, tempConnection) {
-    if (err) {
-      console.log("Error is: ", err.code);
-      tempConnection.release((error) => (error ? reject(error) : resolve()));
-      return;
-    }
-    const sql = "CALL Proc_Submission_GetNotScored";
+async function Scoring(promiseDatabase) {
+  const submissions = await getNotScoredSubmissions(promiseDatabase);
+  if (submissions.length == 0) {
+    console.log("All submissions is scored. Nothing to do!");
+    return;
+  }
 
-    database.query(sql, function (err, result, fields) {
-      if (err) {
-        console.log("Error is: ", err.code);
-        tempConnection.release((error) => (error ? reject(error) : resolve()));
+  console.log("[1] Getting submissions not scored...");
+  console.log("Number of submissions: ", submissions.length);
 
-        return;
-      }
-      const data = result[0];
+  console.log("[2] Export data to json file");
+  exportJSONFile(submissions);
 
-      console.log("this is data length: ", data.length);
-      if (data.length == 0) {
-        console.log("Every submission is scored. Nothing to do!");
-        tempConnection.release((error) => (error ? reject(error) : resolve()));
-        return;
-      }
+  console.log("[3] Cypress run");
+  shell.exec("npx cypress run --spec cypress/e2e/scoring/scoring.cy.js");
+  console.log("Cypress Done!!!");
 
-      console.log("[1] Getting submissions not scored...");
-      exportJSONFile(data);
+  console.log("[4] Reading cypress json report...");
+  let reports = readReport();
 
-      // cypress run
-      console.log("[3] Cypress run");
-      shell.exec("npx cypress run --spec cypress/e2e/scoring/scoring.cy.js");
-      console.log("Cypress Done!!!");
-
-      let reports = readReport();
-      // const res = async () =>
-      //   await ScoringSubmissions(reports, database, tempConnection);
-      ScoringSubmissions(reports, database, tempConnection);
-      // res();
-    });
-  });
+  console.log("Analysis results report...");
+  await AnalysisReportAndUpdateScore(promiseDatabase, reports);
+  console.log("\nScoring done!!! The End.");
 }
