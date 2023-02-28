1c166a934d1b5c99922fc311d493b60946dfd332
duyen0207 <duyeenlee2072001@gmail.com>
2023-02-28 18:22:56 +0700

    chore: clean code

--- cypress/e2e/scoring/scoring2.cy.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-
-import data from "../../../data/submissions.json";
-import LAB_LETTER from "../problems/Letter.cy";
-
-for (let i = 0; i < data.length; i++) {
-  LAB_LETTER(data[i].SubmittedLink, data[i]);
-}
